005e60fb09ee8d76dbbe13e56a3ad670c082ee73
Strato <benjamin@thiebaut.me>
2025-02-14 14:55:14 +0100

    Rebuild projet for .net 6

--- ModEntry.cs
+++ ModEntry.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using GenericModConfigMenu;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using StardewModdingAPI;
@@ -36,7 +37,13 @@ namespace RunningKey
         public int StaminaLimit { get; set; } = 20;
 
         /// <summary>
+        /// Enable run with left stick on gamepad
+        /// </summary>
+        public bool EnableGamePad { get; set; } = true;
+
+        /// <summary>
         /// Reduction gain speed depends on game, reduction is removed to base gain
+        /// @deprecated 
         /// </summary>
         public int RainReductionPercent { get; set; } = 20;
         public int SnowReductionPercent { get; set; } = 30;
@@ -44,6 +51,7 @@ namespace RunningKey
 
         /// <summary>
         /// Tile specific gain percent
+        /// @deprecated
         /// </summary>
         public int FlooredGainPercent { get; set; } = 10;
 
@@ -52,32 +60,84 @@ namespace RunningKey
     public class ModEntry : Mod
     {
         private ModConfig Config;
-        private int BaseSpeed;
-        private float FinalAdded;
-        private int BaseStamina;
-        private int FinalMinStamina;
+        private float baseSpeed;
+        private float finalAdded;
+        private int baseStamina;
+        private int finalMinStamina;
         private int lastTickLose;
-        private bool IsRunning { get; set; } = false;
+        private bool isRunning { get; set; } = false;
         private int originalSpeed = 0;
-        private InputButton gamepadRun;
-        private int GameTickTimeout { get; set; } = 30;
-        private bool WasRunningPad { get; set; } = false;
-        private static int TickRunningTimeout = 15;
-        private bool runPressed = false;
+        private bool wasRunningPad { get; set; } = false;
 
         public override void Entry(IModHelper helper)
         {
             this.Config = helper.ReadConfig<ModConfig>();
 
+            helper.Events.GameLoop.GameLaunched += this.OnGameLaunched;
             helper.Events.GameLoop.UpdateTicked += this.OnUpdateTicked;
             helper.Events.GameLoop.DayStarted   += this.DayStart;
             helper.Events.GameLoop.DayEnding    += this.DayEnd;
             helper.Events.GameLoop.SaveLoaded   += this.SaveLoaded;
         }
 
+        private void OnGameLaunched(object sender, GameLaunchedEventArgs e)
+        {
+            var configMenu = this.Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+            if (configMenu is null)
+                return;
+
+            configMenu.AddNumberOption(
+                mod: this.ModManifest,
+                name: () => "Running speed gain",
+                tooltip: () => "% of speed gainned when pressing the running button",
+                getValue: () => this.Config.RunningSpeedGain,
+                setValue: value => this.Config.RunningSpeedGain = value
+                );
+
+            configMenu.AddKeybind(
+                mod: this.ModManifest,
+                name: () => "Assigned key",
+                tooltip: () => "Key to run, be careful to unset leftshift key in original game key binding if you choose this one",
+                getValue: () => this.Config.Key,
+                setValue: value => this.Config.Key = value
+                );
+
+            configMenu.AddNumberOption(
+                mod: this.ModManifest,
+                name: () => "Stamina losed over time",
+                tooltip: () => "% of stamina leached from running, calculated at rate tick in configuration",
+                getValue: () => this.Config.StaminaLose,
+                setValue: value => this.Config.StaminaLose = value
+            );
+
+
+            configMenu.AddNumberOption(
+                mod: this.ModManifest,
+                name: () => "Lose stamina every x tick",
+                getValue: () => this.Config.StaminaTick,
+                setValue: value => this.Config.StaminaTick = value
+            );
+
+            configMenu.AddNumberOption(
+                mod: this.ModManifest,
+                name: () => "Limit stamina required to run",
+                tooltip: () => "If this limit is reached, run will be disabled",
+                getValue: () => this.Config.StaminaLimit,
+                setValue: value => this.Config.StaminaLimit = value
+            );
+
+            configMenu.AddBoolOption(
+                mod: this.ModManifest,
+                name: () => "Enable gamepad left stick running option",
+                getValue: () => this.Config.EnableGamePad,
+                setValue: value => this.Config.EnableGamePad = value
+                );
+
+        }
+
         private void SaveLoaded(object sender, SaveLoadedEventArgs e)
         {
-            BaseSpeed = Game1.player.addedSpeed;
+            baseSpeed = Game1.player.addedSpeed;
         }
 
         private void OnUpdateTicked(object sender, UpdateTickedEventArgs e)
@@ -88,27 +148,33 @@ namespace RunningKey
             GamePadState currentState = GamePad.GetState(PlayerIndex.One);
 
             bool keyRun = this.Helper.Input.IsDown(this.Config.Key);
-            bool isMovingThumb = currentState.ThumbSticks.Left.X > 0.1f || currentState.ThumbSticks.Left.X < -0.1f || currentState.ThumbSticks.Left.Y > 0.1f || currentState.ThumbSticks.Left.Y < -0.1f;
-            this.runPressed = currentState.IsConnected && currentState.Buttons.LeftStick == ButtonState.Pressed;
+            bool isMovingThumb = false;
+            bool runPressed = false;
 
-            if (IsRunning && isMovingThumb)
+            if (this.Config.EnableGamePad)
             {
-                WasRunningPad = true;
-            }else if (runPressed)
-            {
-                WasRunningPad = true;
-            }else
-            {
-                WasRunningPad = false;
-                IsRunning = false;
+                isMovingThumb = currentState.ThumbSticks.Left.X > 0.1f || currentState.ThumbSticks.Left.X < -0.1f || currentState.ThumbSticks.Left.Y > 0.1f || currentState.ThumbSticks.Left.Y < -0.1f;
+                runPressed = currentState.IsConnected && currentState.Buttons.LeftStick == ButtonState.Pressed;
             }
 
-            if ((WasRunningPad || keyRun) && Game1.player.Stamina > FinalMinStamina)
+            if (isRunning && isMovingThumb)
+            {
+                wasRunningPad = true;
+            }else if (runPressed)
+            {
+                wasRunningPad = true;
+            }else
+            {
+                wasRunningPad = false;
+                isRunning = false;
+            }
+
+            if ((wasRunningPad || keyRun) && Game1.player.Stamina > finalMinStamina)
             {
                 run();
-            }else if (!IsRunning)
+            }else if (!isRunning)
             {
-                Game1.player.addedSpeed = BaseSpeed;
+                Game1.player.addedSpeed = baseSpeed;
             }
         }
 
@@ -118,11 +184,11 @@ namespace RunningKey
 
             CalculateFinalSpeed();
 
-            if (FinalAdded > 0)
+            if (finalAdded > 0)
             {
                 //Game1.player.Speed = (int)FinalAdded + 5;
-                Game1.player.addedSpeed = (int)FinalAdded;
-                this.IsRunning = true;
+                Game1.player.addedSpeed = (int)finalAdded;
+                this.isRunning = true;
 
 
                 if (lastTickLose + this.Config.StaminaTick < Game1.ticks)
@@ -132,7 +198,7 @@ namespace RunningKey
                 }
             }
             else
-                IsRunning = false;
+                isRunning = false;
         }
 
         private void DayStart(object sender, DayStartedEventArgs e)
@@ -141,8 +207,8 @@ namespace RunningKey
             //BaseSpeed = 5f;
 
             // Stamina reference
-            BaseStamina = Game1.player.MaxStamina;
-            FinalMinStamina = (BaseStamina * this.Config.StaminaLimit) / 100;
+            baseStamina = Game1.player.MaxStamina;
+            finalMinStamina = (baseStamina * this.Config.StaminaLimit) / 100;
         }
 
         private void DayEnd(object sender, DayEndingEventArgs e)
@@ -154,10 +220,10 @@ namespace RunningKey
 
         private void CalculateFinalSpeed()
         {
-            FinalAdded = addPercent(5, this.Config.RunningSpeedGain) - 5;
-            if (FinalAdded < 0)
+            finalAdded = addPercent(5, this.Config.RunningSpeedGain) - 5;
+            if (finalAdded < 0)
             {
-                FinalAdded = FinalAdded * -1;
+                finalAdded = finalAdded * -1;
             }
             /*
             Vector2 TileLocation = Game1.player.getTileLocation();
@@ -182,8 +248,8 @@ namespace RunningKey
                 }
             }
             */
-            var finalSpeed = FinalAdded - BaseSpeed;
-            FinalAdded = finalSpeed > 0 ? finalSpeed : 0;
+            var finalSpeed = finalAdded - baseSpeed;
+            finalAdded = finalSpeed > 0 ? finalSpeed : 0;
         }
 
         private float addPercent(float baseNumber, int percent)
